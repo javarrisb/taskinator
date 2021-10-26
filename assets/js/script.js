06f13cc65aec8c1153ab9395ce45a5f6945043e8
Javarris Barnett <javarrisb@gmail.con>
2021-10-26 20:35:33 +0200

    captured delete button from task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,30 @@ var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var pageContentEl = document.querySelector("#page-content");
+
+// other logic...
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+    }
+
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
 var taskFormHandler = function(event) {
      event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
